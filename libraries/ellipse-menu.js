2044ca219cdb1bd66e2a8470f7bd0768d8aef94a
Vijayasekhar Yeruva <vijayasekhar.yeruva@gmail.com>
2020-03-06 17:04:28 -0500

    finished ellipse button click

--- libraries/ellipse-menu.js
+++ libraries/ellipse-menu.js
@@ -8,52 +8,75 @@ let button5 = document.getElementById("embutton5");
 let button6 = document.getElementById("embutton6");
 let desc = document.getElementById("ellipse-item-desc");
 
-function displayDesc(button)
+function clickHandler(button)
 {
-    button.classList.toggle("clicked");
-    switch(button.id){
-        case "embutton1":
-            desc.innerHTML = "Fellowships provide unique opportunities to conduct novel research,"+
-                             "teach in a classroom, provide community service, or even work in "+
-                             "a public or private sector organization positioning you for future success";
-            break;
-        case "embutton2":
-            desc.innerHTML = "Mentorship will provide important opportunities strengthen your" + 
-                             "skills and knowledge, while providing  professional socialization and networking"+
-                             " and personal support";
-            break;
-        case "embutton3":
-            desc.innerHTML = "These awards shine a light on your talents and abilities, while"+
-                             " enabling you to establish new connections, build confidence, and create new "+
-                             "opportunities for future growth";
-            break;
-        case "embutton4":
-            desc.innerHTML = "Opportunity to acquire new knowledge and skills to enhance your transition"+
-                             " from to further your education or to pursue a future profession";
-            break;
-        case "embutton5":
-            desc.innerHTML = "Fellowships/assistantships provide opportunities for first-hand experience "+
-                             "in research, training, service or work in a real-life setting";
-            break;
-        case "embutton6":
-             desc.innerHTML = "Winning an award provides significant financial support, tuition, travel "+
-                              "support and/or more";
-             break;
-        default:
-            desc.innerHTML = "";
+    unclickPrevious(button.id);
+    if(button.classList.toggle("clicked"))
+    {
+        pic.style.visibility = "hidden";
+        switch(button.id){
+            case "embutton1":
+                desc.innerHTML = "Fellowships provide unique opportunities to conduct novel research,"+
+                                 "teach in a classroom, provide community service, or even work in "+
+                                 "a public or private sector organization positioning you for future success";
+                                 desc.classList.add("embutton1");
+                break;
+            case "embutton2":
+                desc.innerHTML = "Mentorship will provide important opportunities strengthen your" + 
+                                 "skills and knowledge, while providing  professional socialization and networking"+
+                                 " and personal support";
+                                 desc.classList.add("embutton2");
+                break;
+            case "embutton3":
+                desc.innerHTML = "These awards shine a light on your talents and abilities, while"+
+                                 " enabling you to establish new connections, build confidence, and create new "+
+                                 "opportunities for future growth";
+                                 desc.classList.add("embutton3");
+                break;
+            case "embutton4":
+                desc.innerHTML = "Opportunity to acquire new knowledge and skills to enhance your transition"+
+                                 " from to further your education or to pursue a future profession";
+                                 desc.classList.add("embutton4");
+                break;
+            case "embutton5":
+                desc.innerHTML = "Fellowships/assistantships provide opportunities for first-hand experience "+
+                                 "in research, training, service or work in a real-life setting";
+                                 desc.classList.add("embutton5");
+                break;
+            case "embutton6":
+                 desc.innerHTML = "Winning an award provides significant financial support, tuition, travel "+
+                                  "support and/or more";
+                                  desc.classList.add("embutton6");
+                 break;
+            default:
+                desc.innerHTML = "";
+        }
     }
-    pic.style.visibility = "hidden";
+    else
+    {
+        clear();
+    }
 }
 
-clear = function()
+function clear()
 {
+    desc.className = "";
     desc.innerHTML = "";
     pic.style.visibility = "visible";
 }
 
-button1.onmousedown = function() {displayDesc(this)};
-button2.onmousedown = function() {displayDesc(this)};
-button3.onmousedown = function() {displayDesc(this)};
-button4.onmousedown = function() {displayDesc(this)};
-button5.onmousedown = function() {displayDesc(this)};
-button6.onmousedown = function() {displayDesc(this)};
+function unclickPrevious(id){
+    var prev_id = desc.className;
+    if(prev_id  != "" && id != prev_id )
+    {
+        document.getElementById(prev_id).classList.remove("clicked");
+        desc.className = "";
+    }
+}
+
+button1.onclick = function() {clickHandler(this)};
+button2.onclick = function() {clickHandler(this)};
+button3.onclick = function() {clickHandler(this)};
+button4.onclick = function() {clickHandler(this)};
+button5.onclick = function() {clickHandler(this)};
+button6.onclick = function() {clickHandler(this)};
