da322b72f370cc7d703ea9a82c5196b0900cb537
Vijay.Yeruva <Yeruva.Vijay@gmail.com>
2020-03-25 09:54:44 -0400

    updated accordion click logic

--- js/accordion.js
+++ js/accordion.js
@@ -1,30 +1,23 @@
-window.onload = function(){
-    //getting accordion-headers to add click event
-    let accordions = document.getElementsByClassName('accordion-header');
-    if(accordions.length == 0)
-    {
-        this.location.reload(true);
-    }
-    let hideshowfunction = function(){  
-        //logic for checking display of content
-        if(this.nextElementSibling.style.display != "block")
-        {
-            //if content is invisible, set other open content's display to none
-            for (let i = 0; i < accordions.length; i++) {
-                accordions[i].nextElementSibling.style.display = "none";      
+let mainContent = document.getElementsByClassName('main-content')[0];
+mainContent.addEventListener('click', function(e){
+    if(e.target && e.target.parentElement.className == 'accordion-header'){
+            let currentaccordion = e.target.parentElement;
+            let accordions = document.getElementsByClassName('accordion-header');
+            
+            if(currentaccordion.nextElementSibling.style.display != "block")
+	        {
+                //if content is invisible, set other open content's display to none
+                for (let i = 0; i < accordions.length; i++) {
+                    accordions[i].nextElementSibling.style.display = "none";      
+                }
+                //setting current content display to block to show content
+                currentaccordion.nextElementSibling.style.display = "block";
             }
-            //setting current content display to block to show content
-            this.nextElementSibling.style.display = "block";
-        }
-        
-        //If content is not hidden, changing the display property to none to hide content
-        else if(this.nextElementSibling.style.display == "block")
-        {
-            this.nextElementSibling.style.display = "none";
+    
+	    //If content is not hidden, changing the display property to none to hide content
+        else if(currentaccordion.nextElementSibling.style.display == "block")
+	    {
+            currentaccordion.nextElementSibling.style.display = "none";
         }
     }
-    //Adding click event to every accordion-headers
-    for (let i = 0; i < accordions.length; i++) {
-        accordions[i].addEventListener('click', hideshowfunction, false);
-    }
-}
+ })
